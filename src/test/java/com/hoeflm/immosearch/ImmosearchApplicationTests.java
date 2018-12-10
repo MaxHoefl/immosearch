05aebe3e58a148d3e9832369989704574ef9ffeb
MaxHoefl <ucabhoe@ucl.ac.uk>
2018-12-10 21:35:11 +0000

    init

---
+++ src/test/java/com/hoeflm/immosearch/ImmosearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hoeflm.immosearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImmosearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
